95d6ae70e324e3d86d171ed4ff116b01219beaab
Ranjitkumar Burgute <80883340+ranjitburgute@users.noreply.github.com>
2022-05-23 10:21:39 +0530

    code coverage

--- src/main/java/com/example/ParkingApplication.java
+++ src/main/java/com/example/ParkingApplication.java
@@ -10,22 +10,6 @@ public class ParkingApplication {
 
     public static void main(String[] args) {
 
-        Mall mall = new Mall(2, 0, 0);
-
-        Ticket ticket1 = mall.parkVehicle(Constants.BIKE);
-        Date ticket1Entry = ticket1.getEntryTime();
-
-        Ticket ticket2 = mall.parkVehicle(Constants.BIKE);
-        Date ticket2Entry = ticket2.getEntryTime();
-
-        mall.parkVehicle(Constants.BIKE);
-
-        ticket2.setExitTime(new Date(ticket2Entry.getTime() + Constants.HOUR));
-        mall.exitVehicle(ticket2.getTickerNr());
-
-        mall.parkVehicle(Constants.BIKE);
-
-        ticket1.setExitTime(new Date(ticket1Entry.getTime() + 4 * Constants.HOUR));
-        mall.exitVehicle(ticket1.getTickerNr());
+        System.out.println("Run junit testcases with command ./gradlew test");
     }
 }
